f4264086a105c39eb55547ed8d5651240d0b0c1e
Jornada Arquidiocesana Juvenil Tabay 2024 <jajtabay2024@gmail.com>
2023-08-10 17:40:03 -0400

    JAJTabay2024

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './share/header/header.component';
+import { FooterComponent } from './share/footer/footer.component';
+import { Jajmerida2024Component } from './pages/jajmerida2024/jajmerida2024.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    Jajmerida2024Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/jajmerida2024/jajmerida2024.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jajmerida2024',
+  templateUrl: './jajmerida2024.component.html',
+  styleUrls: ['./jajmerida2024.component.css']
+})
+export class Jajmerida2024Component {
+
+}
